44b3b150016b08f185f6774a2cc8afa9c172c070
AndresGutierrez <c0dingw.dre@gmail.com>
2022-04-06 17:26:19 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Andres Gutierrez on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
